952147ecc052287f31ef14b6080e21b4472fd25f
r-liciotti <35048026+r-liciotti@users.noreply.github.com>
2025-07-20 14:47:17 +0200

    Update utility.ts
    Get more randomization

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -1,7 +1,19 @@
 import cards from "../data/Card.json";
 import type { ICard } from "../interfaces/Interfaces";
 
+const cardsList = cards;
+
 export function getRandomCard(): ICard {
-    const card = cards[Math.floor(Math.random() * cards.length)];
+    if (cardsList.length === 0) {
+        cardsList.push(...cards);
+    }
+    const card = cardsList[Math.floor(Math.random() * cards.length)];
+    const index = cardsList.indexOf(card);
+    if (index > -1) {
+        cardsList.splice(index, 1);
+    }
     return card;
 }
+
+
+
